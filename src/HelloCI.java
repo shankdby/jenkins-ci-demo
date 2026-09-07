8151970cf8da52aff64e17b851239442664964a6
Shashank Dubey <140532248+shankdby@users.noreply.github.com>
2026-09-07 06:04:46 +0530

    Add HelloCI class for Jenkins CI/CD demo

---
+++ src/HelloCI.java
@@ -0,0 +1,6 @@
+public class HelloCI {
+    public static void main(String[] args) {
+            System.out.println("Hello from Jenkins CI/CD Demo - DevOps Experiment 3");
+                }
+                }
+                
